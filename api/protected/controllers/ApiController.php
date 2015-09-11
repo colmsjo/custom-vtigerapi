39c907a68d092d51477a17ffc3128369774a555d
Prabhat Khera <prabhat.khera@essindia.co.in>
2015-09-11 14:29:19 +0530

    Update ApiController.php

--- api/protected/controllers/ApiController.php
+++ api/protected/controllers/ApiController.php
@@ -307,7 +307,7 @@ class ApiController extends Controller {
                 array(
                     'clientid' => $this->_clientid,
                     'username' => $_SERVER['HTTP_X_USERNAME'],
-                    'password' => $_SERVER['HTTP_X_PASSWORD']
+                    'password' => $_SERVER['HTTP_X_PASSWORD'],
                     'serveraddr'=>$_SERVER['HTTP_X_SERVERADDR']
                 )
         );
